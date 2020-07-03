4edbdf73f4a980744915bfd32a32038ce1003401
Mateusz Neumann <mateumann@gmail.com>
2020-07-03 08:31:45 +0000

    Remove more unused software

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,7 @@
 
 systemctl stop dphys-swapfile
 systemctl disable dphys-swapfile
-apt remove --purge -y lxterminal lxtask dphys-swapfile dc rpi-eeprom-images lightdm gtk2-engines lxde lxde-common lxde-icon-theme x11-common x11-xserver-utils x11-xkb-utils x11-utils x11-common
+apt remove --purge -y lxterminal lxtask dphys-swapfile dc rpi-eeprom-images lightdm gtk2-engines lxde lxde-common lxde-icon-theme x11-common x11-xserver-utils x11-xkb-utils x11-utils x11-common fontconfig gsettings-desktop-schemas gir1.2-glib-2.0 libgles2-mesa emacsen-common nano fontconfig-config fonts-dejavu-core fonts-droid-fallback fonts-liberation fonts-liberation2 fonts-noto-mono
 apt --purge -y autoremove
 apt install -y  zsh atop docker.io aptitude
 # TODO
