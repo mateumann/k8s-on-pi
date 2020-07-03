17cde06fd8d52f1a2395592496a2cae8dda0708c
Mateusz Neumann <mateumann@gmail.com>
2020-07-03 07:41:20 +0000

    Add apt removals and installs

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 
-apt remove --purge lxterminal lxtasks
+systemctl stop dphys-swapfile
+systemctl disable dphys-swapfile
+apt remove --purge lxterminal lxtasks dphys-swapfile dc rpi-eeprom-images lightdm gtk2-engines lxde lxde-common lxde-icon-theme x11-common x11-xserver-utils x11-xkb-utils x11-utils x11-common
+apt --purge autoremove
+apt install -y  zsh atop docker.io aptitude
 # TODO
